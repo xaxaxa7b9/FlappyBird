aa3721a5e8c1a19f1e99a5ee1bbe025dfe48e43d
xaxaxa7b9 <konrad.pikul23@gmail.com>
2022-04-03 20:50:27 +0200

    adding, images, sounds, finished physics

--- app.js
+++ app.js
@@ -1,59 +1,103 @@
-// JavaScript source code
 document.addEventListener('DOMContentLoaded', () => {
 
-    const bird = document.querySelector('.bird');
-    const gameDisplay = document.querySelector('.game-container');
-    const ground = document.querySelector('.ground');
 
-    let birdLeft = 100;
-    let birdBottom = 100;
-    let gravity = 1.5;
+    const ground = document.querySelector('.ground-moving')
+    const bird = document.querySelector('.bird')
+    const game = document.querySelector('.game-container')
+ 
+
+    
+    let gravity = 1
+    let isGameOver = false
+    let gap = 430
+    let acceleration = 0.2
+    let birdLeft = 220
+    let birdBottom = 100
+
+
 
     function startGame() {
 
 
-       
-            birdBottom -= gravity;
-            bird.style.bottom = birdBottom + 'px';
-            bird.style.left = birdLeft + 'px';
-            //gravity += 0.15;
-        
+       // document.getElementById('song').play();
+        birdBottom -= gravity
+        bird.style.bottom = birdBottom + 'px'
+        bird.style.left = birdLeft + 'px'
+        gravity += acceleration
+      
     }
-    let timer = setInterval(startGame, 20);
-            
-       
-    function control(e) {
+        let timerGame = setInterval(startGame, 20)
+
+
+    function lift(e) {
         if (e.keyCode === 32) {
-            jump();
+            jump()
         }
     }
 
-     
+    function jump() {
+        if (birdBottom < 500) birdBottom += 30
+        bird.style.bottom = birdBottom + 'px'
+        gravity = 1
+    }
+    document.addEventListener('keyup', lift)
 
-        function jump()
-        {
-            if (birdBottom < 490) {
 
-                birdBottom += 50;
-                bird.style.bottom = birdBottom + 'px';
-                gravity = 1.5;
+    function generateObstacle() {
+        let obstacleLeft = 500
+        let randomHeight = Math.random() * 80
+        let obstacleBottom = randomHeight
+        const obstacle = document.createElement('div')
+        const topObstacle = document.createElement('div')
+        if (!isGameOver) {
+            obstacle.classList.add("obstacle")
+            topObstacle.classList.add("topObstacle")
+        }
+        game.appendChild(obstacle)
+        game.appendChild(topObstacle)
+        obstacle.style.left = obstacleLeft + 'px'
+        topObstacle.style.left = obstacleLeft + 'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+        topObstacle.style.bottom = obstacleBottom + gap + 'px'
+
+        function obsMovement() {
+            obstacleLeft -= 2
+            obstacle.style.left = obstacleLeft + 'px'
+            topObstacle.style.left = obstacleLeft + 'px'
+
+            if (obstacleLeft === -60) {
+                clearInterval(timer)
+                game.removeChild(obstacle)
+                game.removeChild(topObstacle)
+            }
+            if (
+                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
+                (birdBottom < obstacleBottom + 150 || birdBottom > obstacleBottom + gap - 200) ||
+                birdBottom === 0 || birdBottom < 0
+            ) {
+                gameOver()
+                clearInterval(timer)
             }
         }
-        document.addEventListener('keyup', control);
-
- 
-    function generateObstacle() {
-        let obstacleLeft = 500;
-        let randomHeight = Math.random() * 60;
-        let obstacleBottom = randomHeight;
-        const obstacle = document.createElement('div');
-        obstacle.classList.add('obstacle');
-        gameDisplay.appendChild(obstacle);
-        obstacle.style.left = obstacleLeft + 'px';
-        obstacle.style.bottom = obstacleBottom + 'px';
-
+        let timer = setInterval(obsMovement, 20)
+        if (!isGameOver) setTimeout(generateObstacle, 3000)
 
     }
-    generateObstacle();
+    generateObstacle()
+
+
+    function gameOver() {
+
+        clearInterval(timerGame)
+        isGameOver = true
+        document.removeEventListener('keyup', lift)
+        ground.classList.add('ground')
+        ground.classList.remove('ground-moving')
+    }
+
+   
+        
+       
+    
 
 })
